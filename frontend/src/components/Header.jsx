eeb409065ae4c34edd2e8aa85e99508a05df9a50
candide85 <liamstiduotechnology@gmail.com>
2024-09-19 04:13:21 +0000

    new front config 1

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -23,7 +23,8 @@ const Header = () => {
                         </>
                     :
                         (
-                            <NavLink className=' text-lg p-2' to='/signin' >Sign in</NavLink>                            
+                            // <NavLink className=' text-lg p-2' to='/signin' >Sign in</NavLink> 
+                            ""                       
                         )}
                 </ul>        
             </div>
